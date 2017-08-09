84a9a924fedeb4d5cfc6e5c5436f366c20591cfb
ahmedhamdi <ahmedhamdi@outlook.com>
2017-08-09 20:44:42 +0200

    First

--- UserCreation/src/main/java/newUsers/Base.java
+++ UserCreation/src/main/java/newUsers/Base.java
@@ -2,9 +2,7 @@ package newUsers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Base {
 	
