f08a78d53c8315374bee88afdd83754ba132f3f9
ahmedhamdi <ahmedhamdi@outlook.com>
2017-08-10 11:54:42 +0200

    - Update FireFox Driver - Add Assertions

--- UserCreation/src/test/java/newUsers/CreateUser.java
+++ UserCreation/src/test/java/newUsers/CreateUser.java
@@ -1,18 +1,40 @@
 package newUsers;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CreateUser extends Base {
 
-	WebDriver driver = null;
+	Boolean AssertorNot = false;
+	String UserError = null;
 	/*@Test
 	public void test() {
 		fail("Not yet implemented");
 	}*/
-	@Test
+	@Test 
 	public void Load(){
-		driver = LoadDriver(driver, "C");
+		// Load WebDriver C for Chrome and F for FireFox 
+		driver = LoadDriver(driver, "F");
+		// Open the URL
 		driver.get(baseURL);
+		// Fill in User Data: Name, Email, Password. 
+		FillData(driver);
+		// Assertion
+		if (AssertorNot == true)
+		{
+		UserError = driver.findElement(By.id("user.name.error")).getText().toString();		//System.out.print(UserError);
+		Assert.assertEquals("Assertion Failed",UserError, "Required") ;
+		}
+		else 		{			/* Do nothing*/		}
+	}
+	
+	public void FillData(WebDriver driver){
+		driver.findElement(By.id("name")).sendKeys("TestName");
+		driver.findElement(By.id("email")).sendKeys("Test@Name.com");
+		driver.findElement(By.id("password")).sendKeys("p@ssw@rd");
+		driver.findElement(By.id("confirmationPassword")).sendKeys("p@ssw@rd");
+		driver.findElement(By.xpath("/html/body/div/div/div/form/fieldset/div[5]/button")).click();
 	}
 }
